266c592b0aaaf9129dfe13010fa7cd2558fb42dc
Sajad Torkamani <sajadtorkamani1@gmail.com>
2022-04-23 11:10:00 +0100

    Setup Bootstrap

--- config/importmap.rb
+++ config/importmap.rb
@@ -2,3 +2,5 @@
 
 pin 'application', preload: true
 pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
+pin "bootstrap", to: "https://ga.jspm.io/npm:bootstrap@5.1.3/dist/js/bootstrap.esm.js"
+pin "@popperjs/core", to: "https://ga.jspm.io/npm:@popperjs/core@2.11.5/lib/index.js"

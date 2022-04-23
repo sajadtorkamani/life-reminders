fee284988a0f88cb99abae19082fed604b2918bf
Sajad Torkamani <sajadtorkamani1@gmail.com>
2022-04-23 12:47:00 +0100

    Add test for logout

---
+++ spec/features/account/logout_spec.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Logout', type: :feature do
+  it 'user can logout successfully' do
+    user = create(:user)
+    login_as(user)
+
+    visit root_path
+    click_on user.truncated_email
+    click_on 'Logout'
+
+    expect(page).to have_text logged_out_message
+  end
+
+  def logged_out_message
+    I18n.t('devise.sessions.signed_out')
+  end
+end

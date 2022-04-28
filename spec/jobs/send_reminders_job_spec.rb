ab1b1b965345b042cc6f7f319d3bb4eea461fab7
Sajad Torkamani <sajadtorkamani1@gmail.com>
2022-04-28 11:34:10 +0100

    Tidy test code

--- spec/jobs/send_reminders_job_spec.rb
+++ spec/jobs/send_reminders_job_spec.rb
@@ -4,16 +4,15 @@ require 'rails_helper'
 
 RSpec.describe SendRemindersJob, type: :job do
   it 'sends a reminder email to users who have a reminder configured' do
-    jim = create(:user)
-    jim_reminder = create(:reminder, :with_notes, user: jim)
-    _bob = create(:user)
+    user_with_reminder = create(:user)
+    create(:reminder, :with_notes, user: user_with_reminder)
+    _user_without_reminder = create(:user)
 
     described_class.perform_now
 
-    emails_sent = ActionMailer::Base.deliveries
-    expect(emails_sent.count).to eq 1
-    expect(emails_sent.map(&:to).flatten).to eq [jim.email]
-    expect(emails_sent.map(&:subject)).to eq [jim_reminder.name]
+    expect(sent_emails.count).to eq 1
+    expect(sent_emails.map(&:to).flatten).to eq [user_with_reminder.email]
+    expect(sent_emails.map(&:subject)).to eq [user_with_reminder.reminders.first.name]
   end
 
   it "doesn't send a reminder email to users who have a reminder configured but have no notes" do
